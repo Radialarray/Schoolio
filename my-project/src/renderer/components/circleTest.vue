dd3ba8a3e64e05907e5f2820bf228da0aa02ddcf
Sven <sven.loechner@gmx.de>
2018-01-14 16:23:17 +0100

    Drag and drop with sorting by values

--- my-project/src/renderer/components/circleTest.vue
+++ my-project/src/renderer/components/circleTest.vue
@@ -1,20 +1,29 @@
 <template>
+
 <div class="uk-width-1-2@m">
+  <button class="uk-button uk-button-default" type="button">Hover</button>
+<div uk-dropdown>
+    <ul class="uk-nav uk-dropdown-nav">
+        <li @click="sortdesc()"><a>Desc</a></li>
+        <li @click="sortasc()"><a>Asc</a></li>
+
+    </ul>
+</div>
   <div class="uk-grid-collapse uk-child-width-1-5@s uk-flex-left uk-text-center" uk-grid>
 
 
 
     <draggable v-model="items" :options="{group:'people'}" @start="drag=true" @end="drag=false">
-       <!-- <div v-for="item in items" :key="item.message">{{item.message}}</div> -->
+      <!-- <div v-for="item in items" :key="item.message">{{item.message}}</div> -->
 
-       <div v-for="(item, index) in items" class="uk-card uk-card-default uk-card-body border">
-             <div class="centerAll" :title="item.message +' ist bei ' + item.val + '%'" uk-tooltip>
-               <v-progress-circular class="centerAll" v-bind:size="80" v-bind:width="10" v-bind:rotate="360" v-bind:value="item.val" color="teal">
-                 <img class="img-rounded" :src="imageLink" />
-               </v-progress-circular>
-             </div>
-             {{ item.message }}
-           </div>
+      <div v-for="(item, index) in items" class="uk-card uk-card-default uk-card-body border">
+        <div class="centerAll" :title="item.message +' ist bei ' + item.val + '%'" uk-tooltip>
+          <v-progress-circular class="centerAll" v-bind:size="80" v-bind:width="10" v-bind:rotate="360" v-bind:value="item.val" color="teal">
+            <img class="img-rounded" :src="imageLink" />
+          </v-progress-circular>
+        </div>
+        {{ item.message }}
+      </div>
     </draggable>
 
   </div>
@@ -22,43 +31,55 @@
 </template>
 
 <script>
+import _ from 'lodash'
 import draggable from 'vuedraggable'
+import vueFilter from 'vue-filter'
 
 export default {
   name: 'circleTest',
   components: {
-              draggable,
-          },
-            data () {
+    draggable
+  },
+  data() {
     return {
       interval: {},
       imageLink: require('../assets/img/fabian-moller-401639.jpg'),
       parentMessage: 'Parent',
       items: [{
-        img: require('../assets/img/fabian-moller-401639.jpg'),
-        message: 'Marc',
-        val: Math.round(Math.random() * 100)
-      },
-      {
-        img: require('../assets/img/fabian-moller-401639.jpg'),
-        message: 'Jana',
-        val: Math.round(Math.random() * 100)
-      }
+          img: require('../assets/img/fabian-moller-401639.jpg'),
+          message: 'Marc',
+          val: 100
+        },
+        {
+          img: require('../assets/img/fabian-moller-401639.jpg'),
+          message: 'Jana',
+          val: 50
+        },
+        {
+          img: require('../assets/img/fabian-moller-401639.jpg'),
+          message: 'Dennis',
+          val: 30
+        },
+        {
+          img: require('../assets/img/fabian-moller-401639.jpg'),
+          message: 'Peter',
+          val: 10
+        }
       ]
     }
   },
-  beforeDestroy () {
-    clearInterval(this.interval)
+  methods: {
+    sortasc: function () {
+      this.items = _.orderBy(this.items, ['val'], ['asc']);
+    },
+    sortdesc: function () {
+      this.items = _.orderBy(this.items, ['val'], ['desc']);
+    }
   },
-  mounted () {
-    // this.interval = setInterval(() => {
-    //   if (this.value === 100) {
-    //     return (this.value = 0)
-    //   }
-    //   this.value += 10
-    // }, 1000)
-
-    // this.value = Math.random()*100
+  mounted: function() {
+    console.log(this.items)
+    this.items = _.orderBy(this.items, ['val'], ['desc']);
+    console.log(this.items)
   }
 }
 </script>
@@ -71,8 +92,8 @@ export default {
 }
 
 .img-rounded {
-   width:80px;
-   border-radius: 50%;
+  width: 80px;
+  border-radius: 50%;
 }
 
 .border {
